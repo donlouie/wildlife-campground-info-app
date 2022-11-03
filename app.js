604a17f3f272c7bd6db08e30ceaa161709337e9e
Don Louie Gallegos <don_gallegos@outlook.com>
2022-11-03 18:45:18 +0800

    update app.js cloudinary link

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.use(
 				'blob:',
 				'data:',
 				'http://res.cloudinary.com',
-				'https://res.cloudinary.com/douqbebwk/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				'https://res.cloudinary.com/dgt3twa3n/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
 				'https://images.unsplash.com',
 			],
 			fontSrc: ["'self'", ...fontSrcUrls],
